039239cd2d637ba2dbafc95daec4b09c6c3470fe
Renz <johnrenztolentino1028@gmail.com>
2024-12-21 23:40:25 +0800

    test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '4xl': '1876px',
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: "#0a0a0a",
+        gray: "#6b7280",
+        'dark-gray': "#374151",
+        'light-gray': "#9ca3af",
       },
+    },
+    fontFamily: {
+      'open-sans': ['Open Sans', 'sans-serif'],
+      'noto-sans': ['Noto Sans', 'sans-serif'],
+      'playfair-display': ['Playfair Display', 'sans-serif'],
     },
   },
   plugins: [],
