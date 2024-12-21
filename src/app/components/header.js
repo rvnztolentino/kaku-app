b6fc9412de5d2c96fecde0ce219b5af38bbdf7c3
Renz <johnrenztolentino1028@gmail.com>
2024-12-22 00:48:49 +0800

    fixed header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header className="text-gray fixed top-0 left-0 right-0 z-50 p-5 border-b border-slate-300">
     <div className="flex justify-between items-center">
       <div className="flex items-center space-x-2">
-      <button className="font-playfair-display text-dark-gray p-1 hover:bg-light-gray-2 hover:rounded-md transition-all duration-300 ease-in-out">《 kaku 》</button>
+      <button className="text-lg font-playfair-display text-dark-gray p-1 hover:bg-light-gray-2 hover:rounded-md transition-all duration-300 ease-in-out">《 kaku 》</button>
       <p className="text-left text-xs text-dark-gray px-2 py-0.5 bg-red-200 rounded-md">coming soon!</p>
       </div>
       <nav>
