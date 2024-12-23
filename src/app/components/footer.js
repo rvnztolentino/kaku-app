fc129c65c46a11688407a08dc5c732e317a9d156
Renz <johnrenztolentino1028@gmail.com>
2024-12-23 14:23:17 +0800

    updated UI

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="sticky fixed bg-white bottom-0 left-0 right-0 bg-gray-800 text-gray text-xs py-4 mx-4 mt-[-50px]">
+    <footer className="sticky fixed bg-white bottom-0 left-0 right-0 bg-gray-800 text-gray text-xs py-4 mx-4 mt-[-40px]">
       <div className="flex justify-between items-center">
         <p>© 2024 Kaku. All rights reserved.</p>
         <nav className="flex space-x-4">
