db03db47868fa1b1bd461ff9a3cc5236e519ace0
Renz <johnrenztolentino1028@gmail.com>
2024-12-24 20:37:58 +0800

    UI update: new home page, moved main page to /contact, UI changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-h-screen font-noto-sans py-10 xs:mt-[-40px] lg:mt-[-30px]">
+      <div className="flex flex-col justify-center items-center min-h-screen font-noto-sans py-10 xs:mt-[-40px] lg:mt-[-40px]">
         <h1 className="text-xl text-dark-gray font-playfair-display">《 kaku 》</h1>
-        <p className="text-5xl text-dark-gray text-center xs:mx-20 lg:mx-96 mt-2">Plan your tasks. Simplify your day. Get more done.</p>
+        <p className="text-5xl text-dark-gray text-center mt-2 xs:mx-20 lg:mx-96 2xl:mx-96">Plan your tasks. Simplify your day. Get more done.</p>
         <p className="text-md text-dark-gray text-center mt-4 mb-5">where productivity meets simplicity!</p>
         <div className="flex space-x-4 mb-6 xs:text-center xs:block lg:inline-block">
           <Link href="/beta"><button className="bg-black rounded-full hover:bg-gray transition-all duration-300 ease-in-out text-white mt-4 xs:mx-8 lg:mx-0 px-10 py-2" > Get started </button></Link>
