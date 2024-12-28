a428c29a36a0683a7ee997cb0099c0458cdc407c
Renz <johnrenztolentino1028@gmail.com>
2024-12-29 01:03:23 +0800

    update: embeded tally.so form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,10 +3,18 @@ import EmailInput from "../components/EmailInput";
 export default function Contact() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-h-screen font-noto-sans py-10 mt-[-80px]">
+      <div className="flex flex-col justify-center items-center min-h-screen font-noto-sans py-10 mt-[35px]">
         <h1 className="text-4xl text-dark-gray font-playfair-display">《 kaku 》</h1>
         <p className="text-md text-light-gray mt-2 mb-5">where productivity meets simplicity!</p>
-        <EmailInput></EmailInput>
+          <iframe
+            src="https://tally.so/embed/3E84D2?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+            width="100%"
+            height="500"
+            frameBorder="0"
+            title="Tally Form"
+            allowFullScreen
+            className="xs:w-auto lg:w-1/3 2xl:w-1/4"
+          ></iframe>
       </div>
     </>
   );
