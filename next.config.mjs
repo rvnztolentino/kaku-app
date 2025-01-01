019cc929e2dea503727a0b1a04507e67c1ae771b
Renz <johnrenztolentino1028@gmail.com>
2025-01-01 15:30:40 +0800

    update: added PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
+});
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -17,11 +17,49 @@ const geistMono = localFont({
 export const metadata = {
   title: "kaku",
   description: "where productivity meets simplicity!",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs14", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#000" }],
+  authors: [
+    { name: "Renz Tolentino" },
+    {
+      name: "Renz Tolentino",
+      url: "https://github.com/rvnztolentino",
+    },
+    {
+      name: "Bently Rafa",
+      url: "https://github.com/bntlyr",
+    }
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/KAKU-logo-128.png" },
+    { rel: "icon", url: "/KAKU-logo-128.png" },
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="generator" content={metadata.generator} />
+        <link rel="manifest" href={metadata.manifest} />
+        <meta name="keywords" content={metadata.keywords.join(", ")} />
+        {metadata.themeColor.map(({ media, color }, index) => (
+          <meta key={index} name="theme-color" media={media} content={color} />
+        ))}
+        {metadata.authors.map(({ name, url }, index) => (
+          <meta key={index} name="author" content={name} {...(url && { href: url })} />
+        ))}
+        <meta name="viewport" content={metadata.viewport} />
+        {metadata.icons.map(({ rel, url }, index) => (
+          <link key={index} rel={rel} href={url} />
+        ))}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
         {children}
